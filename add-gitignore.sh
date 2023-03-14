873a75f01d2dffb0e1e2a565b3d4acb5c8d849f5
jaked0626 <jakez0626@gmail.com>
2023-03-14 19:42:26 +0900

    squashed some bugs

--- add-gitignore.sh
+++ add-gitignore.sh
@@ -10,6 +10,7 @@ gitignore_path=${2:-~/gitignore}
 
 # identify the programming languages used within the directory
 languages=$(find $directory -type f -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.py' -o -name '*.js' -o -name '*.jl' -o -name '*.rs' -o -name '*.go'  -o -name '*.r' -o -name '*.tex' -o -name '*.tf' -o -name '*.rb' -o -name '*.kt' -o -name '*.dart'| sed 's/.*\.//' | sort | uniq)
+echo "$languages"
 
 # map suffix to formal language names
 for lang in $languages
@@ -28,17 +29,18 @@ do
     r) formal_name="R";;
     tex) formal_name="TeX";;
     tf) formal_name="Terraform";;
-    rb) fromal_name="Ruby";;
+    rb) formal_name="Ruby";;
     kt) formal_name="Kotlin";;
     dart) formal_name="Dart";;
     *) formal_name="Unknown";;
   esac
-  language_names+=("$formal_name")
+  language_names+=( "$formal_name" )
 done
+
 
 # echo the languages used
 echo "The following languages were detected in the directory $directory:"
-echo "$language_names"
+echo "${language_names[@]}"
 
 # create a .gitignore if one doesn't already exist
 if [ ! -f "$directory/.gitignore" ]
@@ -47,14 +49,14 @@ then
 fi
 
 # concatenate contents of gitignore files
-for lang in $language_names
+for lang in ${language_names[@]}
 do
   lang_gitignore_path="${gitignore_path}/${lang}.gitignore" 
   echo "checking if ${lang_gitignore_path} exists"
   if [ -f $lang_gitignore_path ]
   then
     echo "file found!"
-    echo "### git ignore for ${lang}" >> "$directory/.gitignore \n"
+    echo "### git ignore for ${lang}" >> "$directory/.gitignore"
     cat "$lang_gitignore_path" >> "$directory/.gitignore"
   fi
 done
